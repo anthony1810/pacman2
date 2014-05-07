72bdcd70af7d5d21088f142b69b789f793687147
Anthony Tran <qquang269@gmail.com>
2014-05-07 14:27:41 +0700

    add constant.h

---
+++ src/constant.h
@@ -0,0 +1,61 @@
+
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+/*!
+	\file constant.h 
+	\brief this file contains all constant used in the whole program
+*/
+
+#define TITLE "DEBIAN PACMAN"
+#define VERSION  "@version 1.7.5"
+#define CREATOR  "@created by THE THUNDER CORP"
+
+#define TITLE_HEIGHT 3
+
+#define GAME_WIDTH  66
+#define GAME_HEIGHT  35
+#define GAME_STARTY  6
+
+#define COMMAND_HEIGHT  3
+#define COMMAND_STARTY  42
+
+#define sepChar "    "
+#define pacman  " Pacman"
+#define ghost  " Ghost"
+#define pellet  " Pellet"
+#define super_pellet " Super Pellet"
+#define fruit " Fruit"
+
+#define PATH "levels/"
+#define EXTENSION ".pac"
+#define COLOR_BACKGROUND 0
+#define COLOR_WALL       1
+#define COLOR_PACMAN     2
+#define COLOR_GHOST		 3
+#define COLOR_FRUIT  	 4
+#define COLOR_PELLET 	 5
+
+#define MENU_TITLE "== MENU =="
+#define MENU_NEW "New Game"
+#define MENU_HSCORE "High Score"
+#define MENU_EDITOR "Level Editor"
+#define MENU_CREDIT "Credits"
+#define MENU_END "Quit"
+#define MENU_INSTRUCTION "Type 'w' and 's' to point to your desired option, and type 'enter' to select that option"
+
+#define UP 65
+#define DOWN 66
+#define RIGHT 67
+#define LEFT 68
+#define DELAY 400	
+
+#define PELLET_SCORE 100
+#define SUPER_PELLET_SCORE 200
+#define FRUIT_SCORE 300
+#define GHOST_SCORE 400
+
+#define INVULRABLE 1
+#define VULRABLE 0
+#define INVULRABLE_DURATION 6000
+#endif
